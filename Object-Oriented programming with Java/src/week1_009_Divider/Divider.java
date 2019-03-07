96cdeb39f1ebf53ad5252774c3d894062c6fbd0a
Luci <Luci@DESKTOP-B83E1CK>
2019-03-07 21:13:00 +0200

    Exercise 009

---
+++ Object-Oriented programming with Java/src/week1_009_Divider/Divider.java
@@ -0,0 +1,24 @@
+package week1_009_Divider;
+
+import java.util.Scanner;
+
+public class Divider
+{
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Type a number: ");
+		int num1 = Integer.parseInt(sc.nextLine());
+
+		System.out.print("Type another number: ");
+		int num2 = Integer.parseInt(sc.nextLine());
+		
+		double sum = (double)num1 / num2;
+		
+		System.out.println("Division: " + num1 + " / " + num2 + " = " + sum);
+
+	}
+
+}
