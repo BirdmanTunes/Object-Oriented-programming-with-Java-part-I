7d0e541e5a36eb13149d10d98988d35c4564ab5b
Luci <Luci@DESKTOP-B83E1CK>
2019-03-07 20:10:19 +0200

    Initial Commit

---
+++ Object-Oriented programming with Java/src/week1-001.Name/Name.java
@@ -0,0 +1,12 @@
+package week1;
+
+public class Name
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Birdman Tunes");
+
+	}
+
+}
---
+++ Object-Oriented programming with Java/src/week1-003.Spruce/Spruce.java
@@ -0,0 +1,18 @@
+package week3;
+
+public class Spruce
+{
+
+	public static void main(String[] args)
+	{
+		
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("*********");
+        System.out.println("    *");
+
+	}
+
+}
