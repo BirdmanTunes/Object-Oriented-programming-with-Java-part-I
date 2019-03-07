5397fb80364795e902f5d4385f469ebb2591640b
Luci <Luci@DESKTOP-B83E1CK>
2019-03-07 20:55:57 +0200

    Exercise 007

---
+++ Object-Oriented programming with Java/src/week1_007_Multiplication/Multiplication.java
@@ -0,0 +1,16 @@
+package week1_007_Multiplication;
+
+public class Multiplication
+{
+
+	public static void main(String[] args)
+	{
+		int num1 = 277;
+		int num2 = 111;
+		int total = num1 * num2;
+		
+		System.out.println(num1 + " * " + num2 + " = " + total);
+
+	}
+
+}
