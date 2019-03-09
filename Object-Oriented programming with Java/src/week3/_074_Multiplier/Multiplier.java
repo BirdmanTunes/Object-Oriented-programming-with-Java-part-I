09b7518d736313108a89cd10089c425beb7bf3cb
Luci <Luci@DESKTOP-B83E1CK>
2019-03-09 19:56:06 +0200

    Exercise 075

---
+++ Object-Oriented programming with Java/src/week3/_074_Multiplier/Multiplier.java
@@ -0,0 +1,15 @@
+package week3._074_Multiplier;
+
+public class Multiplier {
+	
+	private int number;
+	
+	public Multiplier(int number) {
+		this.number = number;
+	}
+	
+	public int multiply(int otherNumber) {
+		return number * otherNumber;
+	}
+
+}
