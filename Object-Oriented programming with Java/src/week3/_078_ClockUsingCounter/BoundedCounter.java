314117f40e3de36c0a41bfca3b74c1f40be10270
Luci <Luci@DESKTOP-B83E1CK>
2019-03-09 21:21:48 +0200

    Exercise 078

---
+++ Object-Oriented programming with Java/src/week3/_078_ClockUsingCounter/BoundedCounter.java
@@ -0,0 +1,36 @@
+package week3._078_ClockUsingCounter;
+
+public class BoundedCounter {
+
+    private int value;
+    private int upperLimit;
+
+    public BoundedCounter(int upperLimit) {
+        value = 0;
+        this.upperLimit = upperLimit;
+    }
+
+    public void next() {
+    	value++;
+    	if (value > upperLimit) 
+    		value = 0;
+    }
+    
+    public int getValue() {
+    	return value;
+    }
+    
+    public void setValue(int newValue) {
+    	value = newValue;
+    }
+
+    public String toString() {
+    	
+    	if (value < 10) {
+    		return "0" + value;
+    	} else {
+    		return "" + value;
+    	}
+    }
+}
+    
