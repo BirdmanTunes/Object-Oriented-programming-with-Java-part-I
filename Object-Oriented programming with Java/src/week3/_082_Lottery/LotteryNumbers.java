854616c6c8a9f1c7ad6395b1050d4df2948de2b7
Luci <Luci@DESKTOP-B83E1CK>
2019-03-09 22:49:22 +0200

    Exercise 082

---
+++ Object-Oriented programming with Java/src/week3/_082_Lottery/LotteryNumbers.java
@@ -0,0 +1,38 @@
+package week3._082_Lottery;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class LotteryNumbers {
+    private ArrayList<Integer> numbers;
+
+    public LotteryNumbers() {
+        // We'll format a list for the numbers
+        this.numbers = new ArrayList<Integer>();
+        // Draw numbers as LotteryNumbers is created
+        this.drawNumbers();
+    }
+
+    public ArrayList<Integer> numbers() {
+        return this.numbers;
+    }
+
+    public void drawNumbers() {
+    	
+    	Random rand = new Random();
+        
+    	int i = 0;
+    	while (i < 7) {
+    		int draw = 1 + rand.nextInt(39);
+    		if (!containsNumber(draw)) {
+    			numbers.add(draw);
+    			i++;
+    		} 
+    	}
+    		
+    }
+
+    public boolean containsNumber(int number) {
+        return numbers.contains(number);
+    }
+}
