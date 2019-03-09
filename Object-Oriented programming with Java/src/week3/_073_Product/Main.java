3532c9658e1f44105899b28ce344a81032cce622
Luci <Luci@DESKTOP-B83E1CK>
2019-03-09 19:43:29 +0200

    Exercise 073

---
+++ Object-Oriented programming with Java/src/week3/_073_Product/Main.java
@@ -0,0 +1,13 @@
+package week3._073_Product;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Product banana = new Product("Banana", 1.48, 10);
+		
+		banana.printProduct();
+
+	}
+
+}
