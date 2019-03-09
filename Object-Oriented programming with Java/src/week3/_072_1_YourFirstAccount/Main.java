09100468f67aa7d4c54bb4b041e9ba36aeb29fa0
Luci <Luci@DESKTOP-B83E1CK>
2019-03-09 19:27:21 +0200

    Exercise 72

---
+++ Object-Oriented programming with Java/src/week3/_072_1_YourFirstAccount/Main.java
@@ -0,0 +1,14 @@
+package week3._072_1_YourFirstAccount;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Account bartosAccount = new Account("Barto's account",100.00);
+
+		bartosAccount.deposit(20);
+		
+		System.out.println(bartosAccount);
+	}
+
+}
