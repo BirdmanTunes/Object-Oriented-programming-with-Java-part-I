94ed03e488e469f164c3aebc6a975ecd5bc8dc3c
Luci <Luci@DESKTOP-B83E1CK>
2019-03-11 18:10:12 +0200

    Exercise 095

---
+++ Object-Oriented programming with Java/src/week5/_095_Money/Money.java
@@ -0,0 +1,57 @@
+package week5._095_Money;
+
+public class Money {
+
+    private final int euros;
+    private final int cents;
+
+    public Money(int euros, int cents) {
+
+        if (cents > 99) {
+            euros += cents / 100;
+            cents %= 100;
+        }
+
+        this.euros = euros;
+        this.cents = cents;
+    }
+
+    public int euros(){
+        return euros;
+    }
+
+    public int cents(){
+        return cents;
+    }
+
+    public String toString() {
+        String zero = "";
+        if (cents <= 10) {
+            zero = "0";
+        }
+
+        return euros + "." + zero + cents + " €";
+    }
+    
+    public Money plus(Money added) {
+    	return added = new Money(euros + added.euros, cents + added.cents);
+    }
+    
+    public Money minus(Money decremented) {
+    	if (this.less(decremented))
+    		return decremented = new Money(0,0);
+    	else
+    		return decremented = new Money(euros - decremented.euros, cents - decremented.cents);
+    		
+    }
+    
+    public boolean less(Money compared) {
+    	if (this.euros < compared.euros) {
+    		return true;
+    	} else if (this.euros == compared.euros && this.cents < compared.cents) {
+    		return true;
+    	} else {
+			return false;
+		}
+    }
+}
